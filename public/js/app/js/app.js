8f8cd8ec0e5a67a41ccef167783396d12b1ccde2
Jason Jover <jason.jover@iacademy.edu.ph>
2017-03-16 01:17:16 +0800

    Successful transfer

---
+++ public/js/app/js/app.js
@@ -0,0 +1,37 @@
+'use strict';
+
+angular.module('seal-app', [
+  'ngAnimate',
+  'ui.router'
+])
+
+.config(['$stateProvider', '$urlRouterProvider', '$locationProvider',
+        '$httpProvider',
+        function($stateProvider, $urlRouterProvider, $locationProvider,
+                $httpProvider) {
+
+            var templatesPath = 'js/app/partials/';
+            console.log('test');
+            $locationProvider.html5Mode(true);
+
+            $stateProvider
+            .state('home', {
+                url: '/',
+                templateUrl: templatesPath + 'home.html' /* js/app/partials/home.html */
+            })
+            ;
+
+
+            var errorInterceptor = ['$q', '$injector', function($q, $injector) {
+                return {
+                    'responseError': function(rejection) {
+                        if(rejection.status == 500) {
+                            $injector.get('$state').transitionTo('error.servererror');
+                        }
+                        
+                        return $q.reject(rejection);
+                    }
+                };
+            }];
+
+}]);
